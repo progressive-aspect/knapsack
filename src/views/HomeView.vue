0bd47125086aa63c5240fbc076b45bfcf20bee83
yellow <9735381+yellow_GOOD@user.noreply.gitee.com>
2022-03-28 11:21:06 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="cesi">res</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style scoped>
+.cesi{
+  width: 7.5rem;
+  height: 7.5rem;
+  background: red;
+}
+</style>
